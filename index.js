5e3dac1e5d83094546cf75766de70b4f48a29e19
Ridwanul Hoque <rhr06junhoque@gmail.com>
2025-02-21 10:38:30 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,135 @@
+const express = require('express')
+const app = express();
+const cors = require('cors');
+require('dotenv').config()
+const port = process.env.port || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+
+
+app.use(cors());
+app.use(express.json());
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8gt7g.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    const usersCollection = client.db("Task-manager").collection("users")
+
+
+
+
+
+    // users
+
+    app.get('/users', async (req, res) => {
+        console.log(req.headers)
+        const result = await usersCollection.find().toArray();
+        res.send(result)
+    });
+    app.get('/users', async (req, res) => {
+        console.log(req.headers)
+        const result = await usersCollection.find().toArray();
+        res.send(result)
+    });
+
+    // app.get('/users/admin/:email', verifyToken, async (req, res) => {
+    //     const email = req.params.email;
+    //     if (email !== req.decoded.email) {
+    //         return res.status(403).send({ message: 'unauthorized access' })
+    //     }
+    //     const query = { email: email }
+    //     const user = await usersCollection.findOne(query);
+    //     let admin = false;
+    //     if (user) {
+    //         admin = user?.role === 'admin';
+
+    //     }
+    //     res.send({ admin })
+
+    // })
+    // app.patch('/users/admin/:id', verifyToken, async (req, res) => {
+    //     const id = req.params.id;
+    //     const filter = { _id: new ObjectId(id) }
+    //     const updatedDoc = {
+    //         $set: {
+    //             role: 'admin'
+    //         }
+    //     }
+    //     const result = await usersCollection.updateOne(filter, updatedDoc)
+    //     res.send(result)
+    // })
+    app.delete('/users/:id', verifyToken, verifyAdmin, async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) }
+        const result = await usersCollection.deleteOne(query);
+        res.send(result)
+    })
+    app.post('/users', async (req, res) => {
+        const user = req.body;
+        const query = { email: user.email }
+        const existingUser = await usersCollection.findOne(query)
+        if (existingUser) {
+            return res.send({ message: 'user already exists', insertedId: null })
+        }
+        const result = await usersCollection.insertOne(user);
+        res.send(result)
+    })
+
+
+
+
+    app.post('/users', async (req, res) => {
+        const user = req.body;
+        const query = { email: user.email }
+        const existingUser = await usersCollection.findOne(query)
+        if (existingUser) {
+            return res.send({ message: 'user already exists', insertedId: null })
+        }
+        const result = await usersCollection.insertOne(user);
+        res.send(result)
+    })
+
+
+
+
+
+
+
+
+
+
+
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+app.get('/', (req, res) => {
+    res.send('task manager is live');
+})
+
+app.listen(port, () => {
+    console.log(`Task Manger is sitting on Port ${port}`);
+})
